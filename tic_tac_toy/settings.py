1c47bbea10e1bc256004576b5970bf755de22a87
Sridhar Goudu <sridhargoudu7@gmail.com>
2025-07-23 15:13:15 +0530

    Fix DB config to support local and Render environments

--- tic_tac_toy/settings.py
+++ tic_tac_toy/settings.py
@@ -87,14 +87,20 @@ WSGI_APPLICATION = 'tic_tac_toy.wsgi.application'
 
 
 import dj_database_url
-import os
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
